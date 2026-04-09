fb2290f370923bc514eb440987e287c8fa4670b3
jueunseuk <junsu120202@gmail.com>
2026-04-09 15:01:08 +0900

    temp

--- app/src/main/java/com/example/androidprogramming/MainActivity.kt
+++ app/src/main/java/com/example/androidprogramming/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.androidprogramming
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.androidprogramming.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        val USER_ID: String = "2471506"
+        val USER_PASSWORD: String = "1234"
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.loginButton.setOnClickListener {
+            val inputId = binding.inputId.text.toString().trim()
+            val inputPassword = binding.inputPassword.text.toString().trim()
+
+            if (inputId.isEmpty() || inputPassword.isEmpty()) {
+                Toast.makeText(this, "아이디와 비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show()
+            } else if (inputId == USER_ID && inputPassword == USER_PASSWORD) {
+                Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "아이디 또는 비밀번호가 올바르지 않습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
